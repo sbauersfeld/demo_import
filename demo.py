68de6ec6b94fde921780a5a408cd9d403d8c0e3d
sbauersfeld@g.ucla.edu <sbauersfeld@g.ucla.edu>
2022-07-11 02:24:09 +0000

    demo import

---
+++ demo.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+# MAGIC %sh ls -l
+
+# COMMAND ----------
+
+from notebook_utils import double
+print(double(10))
+
+# COMMAND ----------
+
+# MAGIC %sh cp -r . /tmp/testcp
+
+# COMMAND ----------
+
+# MAGIC %sh black notebook_utils.py
+
+# COMMAND ----------
+
+
---
+++ notebook_utils.py
@@ -0,0 +1,3 @@
+# Databricks notebook source
+def double(x):
+    return x * 2
